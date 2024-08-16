9b25a63aadc8a38af37ea73e4b8159172b3ada0f
Iana Barkhatova <yanabarhat@gmail.com>
2024-08-16 14:25:09 +0300

    Sprint-3, HW-5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -31,7 +31,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        //  className= {({isActive}) => (АКТИВНЫЙ ? s.ВСТАВЛЯЙ СТИЛЬ : '')} // делает студент
+                        className={({isActive}) => (isActive ? s.active : '')}
                     >
                         Pre-junior
                     </NavLink>
@@ -39,16 +39,14 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
-                    >
+                        className={({isActive}) => (isActive ? s.active : '')}                    >
                         Junior
                     </NavLink>
                     <NavLink
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
-                    >
+                        className={({isActive}) => (isActive ? s.active : '')}                    >
                         Junior Plus
                     </NavLink>
                 </nav>
