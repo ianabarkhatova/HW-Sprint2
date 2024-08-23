c47ae201a9752698aca2521f6d765e97b8294b12
Iana Barkhatova <yanabarhat@gmail.com>
2024-08-23 09:02:55 +0300

    Sprint-3, HW-6

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -22,7 +22,6 @@ type SuperEditableSpanType = Omit<DefaultInputPropsType, 'type'> & {
     onChangeText?: (value: string) => void
     onEnter?: () => void
     error?: string
-
     spanProps?: DefaultSpanPropsType  & {defaultText?: string}// пропсы для спана
 }
 
@@ -37,24 +36,27 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
     }
 ) => {
     const [editMode, setEditMode] = useState<boolean>(false)
-    const {children, onDoubleClick, className, defaultText, ...restSpanProps} =
+    const {children, onDoubleClick, className,
+        defaultText, ...restSpanProps} =
     spanProps || {}
 
     const onEnterCallback = () => {
         // выключить editMode при нажатии Enter // делают студенты
-
+        setEditMode(false)
         onEnter?.()
     }
+
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
         // выключить editMode при нажатии за пределами инпута // делают студенты
-
+        setEditMode(false)
         onBlur?.(e)
     }
+
     const onDoubleClickCallBack = (
         e: React.MouseEvent<HTMLSpanElement, MouseEvent>
     ) => {
         // включить editMode при двойном клике // делают студенты
-
+        setEditMode(true)
         onDoubleClick?.(e)
     }
 
