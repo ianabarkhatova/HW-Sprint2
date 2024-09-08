280a9e9bbcc4fb647d7f218908dceeca3a78c4c4
Iana Barkhatova <yanabarhat@gmail.com>
2024-09-08 18:24:36 +0300

    Sprint-3, HW-8, logic done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,16 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            // sort() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return [...state].sort((a, b) =>
+                action.payload === 'up'
+                    ? a.name.localeCompare(b.name)
+                    : b.name.localeCompare(a.name)
+            )
         }
         case 'check': {
-            // filter() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return state.filter((u) => u.age >= action.payload)
         }
+
         default:
             return state
     }
