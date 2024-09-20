fe4c2724af200acdbd968c6443f17d6fa5e45002
Iana Barkhatova <yanabarhat@gmail.com>
2024-09-20 11:57:54 +0300

    Sprint-3, HW-10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,9 +2,17 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export type LoadingStateType = {
+    isLoading: boolean,
+}
+
+export const loadingReducer = (state = initState, action: LoadingActionType): LoadingStateType => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
+        case 'CHANGE_LOADING': {
+            return {
+                ...state,
+                isLoading: action.isLoading}
+        }
 
         default:
             return state
