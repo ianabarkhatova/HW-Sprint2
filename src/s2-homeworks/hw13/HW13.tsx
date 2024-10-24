79c052f931f8aa132a3e0cda83db63ba87a3e265
Iana Barkhatova <yanabarhat@gmail.com>
2024-10-24 16:31:21 +0300

    Sprint-5, HW-13-redone-2

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -39,6 +39,7 @@ const HW13 = () => {
                 setCode('Код 200!')
                 setImage(success200)
                 setText("...всё ок)")
+                setInfo('код 200 - обычно означает что скорее всего всё ок)')
 
             })
             .catch((e) => {
@@ -46,16 +47,19 @@ const HW13 = () => {
 
                 if(e.response.status === 500) {
                     setCode('Ошибка 500!')
-                    setText("эмитация ошибки на сервере ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)")
+                    setText("эмитация ошибки на сервере")
+                    setInfo('ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
                     setImage(error500)
                 }
                 if(e.response.status === 400) {
                     setCode('Ошибка 400!')
                     setText("Ты не отправил success в body вообще!")
+                    setInfo('ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
                     setImage(error400)
                 } else {
                     setImage(errorUnknown)
-                    setText(`Error`)
+                    setText(`Error!`)
+                    setInfo(`Error`)
                 }
             }).finally(()=> setInfo(''))
     }
